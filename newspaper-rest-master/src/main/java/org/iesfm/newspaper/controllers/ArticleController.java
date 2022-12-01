3fab6740d9b5c69b3d6e07908a817062d88eaa57
AngelClase <angel.torijasanchezj@iesfm.onmicrosoft.com>
2022-12-01 11:10:03 +0100

    third commit

--- newspaper-rest-master/src/main/java/org/iesfm/newspaper/controllers/ArticleController.java
+++ newspaper-rest-master/src/main/java/org/iesfm/newspaper/controllers/ArticleController.java
@@ -1,11 +1,14 @@
 package org.iesfm.newspaper.controllers;
 
 import org.iesfm.newspaper.controllers.dto.ArticleDto;
+import org.iesfm.newspaper.entity.Article;
 import org.iesfm.newspaper.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,5 +29,27 @@ public class ArticleController {
                             .map(article -> ArticleDto.toDto(article))
                             .collect(Collectors.toList()));
     }
-    
+
+    @GetMapping(path = "/articles/{articleId}")
+    public ResponseEntity<ArticleDto> getArticle(
+            @PathVariable("articleId") int articleId
+    ){
+        Article article = articleService.getArticle(articleId);
+        if (article != null){
+            return ResponseEntity.ok(ArticleDto.toDto(article));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PostMapping(path = "/articles")
+    public ResponseEntity<Void> add(
+            @Valid @RequestBody ArticleDto articleDto
+    ){
+        if (articleService.addArticle(ArticleDto.toEntity(articleDto))){
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
 }
