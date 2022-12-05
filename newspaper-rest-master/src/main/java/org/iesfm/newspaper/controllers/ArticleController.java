9911b68e4fa2163b0433895c629311b968b75329
Angel04 <angeltorija4@gmail.com>
2022-12-05 18:53:26 +0100

    final commit

--- newspaper-rest-master/src/main/java/org/iesfm/newspaper/controllers/ArticleController.java
+++ newspaper-rest-master/src/main/java/org/iesfm/newspaper/controllers/ArticleController.java
@@ -8,7 +8,6 @@ import org.iesfm.newspaper.service.exceptions.ArticleExistException;
 import org.iesfm.newspaper.service.exceptions.ArticleNotFoundExceptions;
 import org.iesfm.newspaper.service.exceptions.SectionNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,72 +34,50 @@ public class ArticleController {
     public ResponseEntity<List<ArticleSectionDto>> getSectionArticles(
           @PathVariable("sectionId") int id,
           @PathVariable("author") String author
-    ){
-        try{
-            return ResponseEntity.ok(
-                    sectionService.articleList(id, author)
-                            .stream()
-                            .map(ArticleSectionDto::toDto)
-                            .collect(Collectors.toList())
-            );
-        } catch (ArticleNotFoundExceptions e){
-            return ResponseEntity.notFound().build();
-        }
+    ) throws ArticleNotFoundExceptions {
+        return ResponseEntity.ok(
+                sectionService.articleList(id, author)
+                        .stream()
+                        .map(ArticleSectionDto::toDto)
+                        .collect(Collectors.toList())
+        );
     }
 
     @GetMapping(path = "/sections/{sectionId}/articles/{author}")
     public ResponseEntity<ArticleSectionDto> getArticleByAuthor(
             @PathVariable("sectionId") int id,
             @PathVariable("author") String author
-    ){
-        try {
-            return ResponseEntity.ok(
-                    ArticleSectionDto.toDto(
-                            (Article) sectionService.articleList(id, author)
-                    )
-            );
-        } catch (SectionNotFoundException | ArticleNotFoundExceptions e){
-            return ResponseEntity.notFound().build();
-        }
+    ) throws SectionNotFoundException, ArticleNotFoundExceptions {
+        return ResponseEntity.ok(
+                ArticleSectionDto.toDto(
+                        (Article) sectionService.articleList(id, author)
+                )
+        );
     }
 
     @PostMapping(path = "/sections/{sectionId}/articles")
     public ResponseEntity<Void> add(
             @Valid @RequestBody ArticleDto articleDto
-    ){
-        try {
-            sectionService.addArticle(ArticleDto.toEntity(articleDto));
-            return ResponseEntity.ok().build();
-        } catch (SectionNotFoundException e){
-            return ResponseEntity.notFound().build();
-        } catch (ArticleExistException e){
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
-        }
+    ) throws SectionNotFoundException, ArticleExistException {
+        sectionService.addArticle(ArticleDto.toEntity(articleDto));
+        return ResponseEntity.ok().build();
     }
 
     @PutMapping(path = "/sections/{sectionId}/articles/{id}")
     public ResponseEntity<Void> updateArticle(
             @PathVariable("articleId") int id,
             @Valid @RequestBody ArticleDto article
-    ){
-        try {
-            sectionService.updateArticle(id, ArticleDto.toEntity(article));
-            return ResponseEntity.ok().build();
-        } catch (SectionNotFoundException | ArticleNotFoundExceptions e){
-            return ResponseEntity.notFound().build();
-        }
+    ) throws SectionNotFoundException, ArticleNotFoundExceptions {
+        sectionService.updateArticle(id, ArticleDto.toEntity(article));
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping(path = "/sections/{sectionId}/articles/{id}")
     public ResponseEntity<Void> delete(
             @PathVariable("articleId") int id
-    ){
-        try {
-            sectionService.deleteArticle(id);
-            return ResponseEntity.ok().build();
-        } catch (SectionNotFoundException | ArticleNotFoundExceptions e){
-            return ResponseEntity.notFound().build();
-        }
+    ) throws SectionNotFoundException, ArticleNotFoundExceptions {
+        sectionService.deleteArticle(id);
+        return ResponseEntity.ok().build();
     }
 
 }
